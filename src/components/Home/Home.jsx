22e274ca851def0e762fc9d57c9326f498046776
aguspeiretti <agupeiretti@gmail.com>
2023-04-11 15:28:15 -0300

    11/04 15:28

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,23 +3,42 @@ import "./home.css";
 import Navbar from "../NavBar/Navbar";
 import { data } from "../../productos";
 import Cards from "../Cards/Cards";
+import promotion from "../../assets/foodImg/promotion.png";
 
 const Home = () => {
   return (
     <div className="homeContainer">
       <Navbar />
+      <div className="search">
+        <input type="text" />
+      </div>
       <div className="first-seccion">
-      <div className="categorys">
-        <button className="category-button special">All</button>
-        <button className="category-button">Rolls</button>
-        <button className="category-button">Salads</button>
-        <button className="category-button">recomended</button>
+        <div className="categorys">
+          <button className="category-button special">All</button>
+          <button className="category-button">Rolls</button>
+          <button className="category-button">Salads</button>
+          <button className="category-button">recomended</button>
         </div>
         <div className="product-container">
-        {data.map((p) => (
-          <Cards product={p} />
-        ))}
-        </div>       
+          {data.map((p) => (
+            <Cards product={p} />
+          ))}
+        </div>
+      </div>
+      <div className="second-seccion">
+        <div className="day-sales">
+          <p>Day's promotion</p>
+          <span>$3000</span>
+        </div>
+        <div className="promotion-img">
+          <img src={promotion} alt="" />
+        </div>
+      </div>
+      <div className="thirt-seccion">
+        <div className="suggestion">
+          <p>suggestion for you</p>
+        </div>
+        <div className="suggested"></div>
       </div>
     </div>
   );
