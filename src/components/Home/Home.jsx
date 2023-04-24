13275b73caa5da0c19bf3427a5bdd68a0a858f10
aguspeiretti <agupeiretti@gmail.com>
2023-04-23 21:55:50 -0300

    productButtons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import Navbar from "../NavBar/Navbar";
 import { data } from "../../productos";
@@ -7,30 +7,65 @@ import promotion from "../../assets/foodImg/promotion.png";
 import CardSug from "../CardSuggestion/CardSug";
 import { motion } from "framer-motion";
 
+const all = data;
+const rolls = data.filter((p) => p.type === "Roll");
+const salad = data.filter((p) => p.type === "Salad");
+const combo = data.filter((p) => p.type === "Combo");
+
 const Home = () => {
+  const [products, setProducts] = useState(all);
+
   return (
     <div className="homeContainer">
       <div className="navBarHome">
         <Navbar />
       </div>
-
       <div className="search">
         <input type="text" />
       </div>
       <div className="first-seccion">
         <div className="categorys">
-          <button className="category-button special">All</button>
-          <button className="category-button">Rolls</button>
-          <button className="category-button">Salads</button>
-          <button className="category-button">Combos</button>
+          <button
+            onClick={() => {
+              setProducts(all);
+            }}
+            className="category-button special"
+          >
+            All
+          </button>
+          <button
+            onClick={() => {
+              setProducts(rolls);
+            }}
+            className="category-button"
+          >
+            Rolls
+          </button>
+          <button
+            onClick={() => {
+              setProducts(salad);
+            }}
+            className="category-button"
+          >
+            Salads
+          </button>
+          <button
+            onClick={() => {
+              setProducts(combo);
+            }}
+            className="category-button"
+          >
+            Combos
+          </button>
         </div>
+
         <motion.div className="carrousel">
           <motion.div
             drag="x"
             dragConstraints={{ right: 0, left: -960 }}
             className="product-container"
           >
-            {data.map((p) => (
+            {products.map((p) => (
               <Cards key={p.name} product={p} />
             ))}
           </motion.div>
